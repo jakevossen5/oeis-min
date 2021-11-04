e127c26458b9a39e6911afcc2202ddc6d08d3595
Jake Vossen <jake@vossen.dev>
2021-11-04 10:32:00 -0600

    updated to use oeis-utils 0.3

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ struct Opt {
 fn main() {
     let opt = Opt::from_args();
     let oeis_db = oeis_utils::OEISDatabase::from_path(&opt.input).unwrap();
-    let ss = oeis_db.series;
+    let ss = oeis_db.series();
     let all_nums: HashSet<i128> = ss
         .iter()
         .flat_map(|s| s.values())
         .filter_map(|e| match e {
-            NumberValue::InRange(i) => Some(i),
+            NumberValue::InRange(i) => Some(*i),
             NumberValue::OutOfRange(_) => None, // If we don't fit in a i128 we are not going to be the smallest number
         })
         .collect();
